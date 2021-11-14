59890eef4e781097b0a8d9d106afdcce20365298
Ethan-Shapiro <46407744+Ethan-Shapiro@users.noreply.github.com>
2021-11-14 12:43:29 -0800

    Created Data Validator

--- tests/test_RawDataWrangler.py
+++ tests/test_RawDataWrangler.py
@@ -6,5 +6,18 @@ region = 'na1'
 username = 'Sasheemy'
 
 
-def test_rawdatawrangler():
+def test_constructor():
     assert type(RawDataWrangler(region, username)) == RawDataWrangler
+
+
+rdw = RawDataWrangler(region, username)
+
+
+def test_set_region():
+    assert rdw.set_region('notvalid1') == False
+    assert rdw.set_region('na1') == True
+    assert rdw.set_region('br1') == True
+
+
+def test_get_raw_match_timelines():
+    pass
